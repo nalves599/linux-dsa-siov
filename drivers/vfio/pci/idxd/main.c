d7dac86f8ae556cfe786df36aa2ef5d18eca3d0f
Nuno Alves <nuno.f.alves@tecnico.ulisboa.pt>
2026-04-28 20:03:11 +0100

    vfio/idxd: emulate BAR0 registers
    Add BAR0 register and capability emulation for the IDXD VFIO VDEV.
    
    Expose the guest-visible DSA capability and configuration register layout,
    including group and WQ configuration state, and emulate the command/status
    paths needed by the guest idxd driver. Enforce the initial WQ configuration
    policy by making shared WQ configuration host-owned while allowing exclusive
    VDEV WQs to expose guest-controlled fields.
    
    This gives the guest driver enough of the physical DSA register interface to
    probe and manage the virtual device.

--- drivers/vfio/pci/idxd/main.c
+++ drivers/vfio/pci/idxd/main.c
@@ -2,6 +2,7 @@
 /* Copyright(c) 2026 Intel Corporation */
 
 #include <linux/capability.h>
+#include <linux/bitmap.h>
 #include <linux/container_of.h>
 #include <linux/device.h>
 #include <linux/io.h>
@@ -22,16 +23,40 @@
 #define IDXD_VDEV_BAR0_SIZE		SZ_64K
 #define IDXD_VDEV_PORTALS_PER_WQ	2
 #define IDXD_VDEV_WQCFG_OFFSET		0x100
-#define IDXD_VDEV_GRPCFG_OFFSET	0x200
-#define IDXD_VDEV_MSIX_PERM_OFFSET	0x300
-#define IDXD_VDEV_IMS_OFFSET		0x400
+
+#define IDXD_VDEV_CMDCAP (BIT(IDXD_CMD_ENABLE_DEVICE) |		\
+			  BIT(IDXD_CMD_DISABLE_DEVICE) |		\
+			  BIT(IDXD_CMD_DRAIN_ALL) |		\
+			  BIT(IDXD_CMD_ABORT_ALL) |		\
+			  BIT(IDXD_CMD_RESET_DEVICE) |		\
+			  BIT(IDXD_CMD_ENABLE_WQ) |		\
+			  BIT(IDXD_CMD_DISABLE_WQ) |		\
+			  BIT(IDXD_CMD_DRAIN_WQ) |		\
+			  BIT(IDXD_CMD_ABORT_WQ) |		\
+			  BIT(IDXD_CMD_RESET_WQ) |		\
+			  BIT(IDXD_CMD_DRAIN_PASID) |		\
+			  BIT(IDXD_CMD_ABORT_PASID))
+
+#define IDXD_VDEV_WQCFG_IDX2_WR_MASK (BIT(0) | GENMASK(27, 8) |	\
+				       BIT(28) | BIT(29))
 
 struct idxd_vfio_device {
 	struct vfio_device vdev;
 	struct idxd_vdev *ivdev;
 	u8 config[PCI_CFG_SPACE_EXP_SIZE];
+
+	struct mutex bar0_lock;	/* protects the virtual BAR0 state */
+	enum idxd_device_status_state state;
+	union wqcfg *wqcfg;
+	unsigned long *wq_enable_map;
+	struct grpcfg grpcfg;
 	u32 genctrl;
 	u32 gencfg;
+	u32 intcause;
+	u32 cmdsts;
+	u64 evlcfg[2];
+	u64 evlstatus;
+
 	struct mutex pasid_lock;	/* protects default_host_pasid */
 	u32 default_host_pasid;
 	bool pasid_attached;
@@ -40,6 +65,34 @@ struct idxd_vfio_device {
 static size_t idxd_vfio_bar2_size(struct idxd_vfio_device *vfio_dev)
 {
 	return vfio_dev->ivdev->num_wqs * IDXD_VDEV_PORTALS_PER_WQ * PAGE_SIZE;
+}
+
+static u32 idxd_vfio_wqcfg_offset(struct idxd_vfio_device *vfio_dev)
+{
+	return IDXD_VDEV_WQCFG_OFFSET;
+}
+
+static u32 idxd_vfio_wqcfg_size(struct idxd_vfio_device *vfio_dev)
+{
+	struct idxd_vdev *ivdev = vfio_dev->ivdev;
+
+	return ivdev->num_wqs * ivdev->idxd->wqcfg_size;
+}
+
+static u32 idxd_vfio_grpcfg_offset(struct idxd_vfio_device *vfio_dev)
+{
+	return ALIGN(idxd_vfio_wqcfg_offset(vfio_dev) +
+		     idxd_vfio_wqcfg_size(vfio_dev), IDXD_TABLE_MULT);
+}
+
+static u32 idxd_vfio_msix_perm_offset(struct idxd_vfio_device *vfio_dev)
+{
+	return idxd_vfio_grpcfg_offset(vfio_dev) + IDXD_TABLE_MULT;
+}
+
+static u32 idxd_vfio_ims_offset(struct idxd_vfio_device *vfio_dev)
+{
+	return idxd_vfio_msix_perm_offset(vfio_dev) + IDXD_TABLE_MULT;
 }
 
 static void idxd_vfio_config_writew(struct idxd_vfio_device *vfio_dev,
@@ -107,10 +160,16 @@ static u64 idxd_vfio_read_wqcap(struct idxd_vdev *ivdev)
 
 	list_for_each_entry(vwq, &ivdev->wqs, node) {
 		wqcap.total_wq_size += vwq->wq->size;
-		if (vwq->shared)
+		if (vwq->shared) {
 			wqcap.shared_mode = 1;
-		else
+		} else {
+			/*
+			 * A single-guest assignment can be exposed to the guest
+			 * as either a dedicated or shared WQ.
+			 */
 			wqcap.dedicated_mode = 1;
+			wqcap.shared_mode = 1;
+		}
 	}
 
 	return wqcap.bits;
@@ -120,6 +179,9 @@ static u64 idxd_vfio_read_gencap(struct idxd_vdev *ivdev)
 {
 	union gen_cap_reg gencap = ivdev->idxd->hw.gen_cap;
 
+	gencap.cmd_cap = 1;
+	gencap.evl_support = 0;
+	gencap.max_ims_mult = 0;
 	gencap.config_en = 0;
 	return gencap.bits;
 }
@@ -141,30 +203,35 @@ static u64 idxd_vfio_read_enginecap(struct idxd_vdev *ivdev)
 	return enginecap.bits;
 }
 
-static u64 idxd_vfio_read_table(unsigned int idx)
+static u64 idxd_vfio_read_table(struct idxd_vfio_device *vfio_dev,
+				unsigned int idx)
 {
 	union offsets_reg offsets = {};
 
-	offsets.grpcfg = IDXD_VDEV_GRPCFG_OFFSET / IDXD_TABLE_MULT;
-	offsets.wqcfg = IDXD_VDEV_WQCFG_OFFSET / IDXD_TABLE_MULT;
-	offsets.msix_perm = IDXD_VDEV_MSIX_PERM_OFFSET / IDXD_TABLE_MULT;
-	offsets.ims = IDXD_VDEV_IMS_OFFSET / IDXD_TABLE_MULT;
+	offsets.grpcfg = idxd_vfio_grpcfg_offset(vfio_dev) / IDXD_TABLE_MULT;
+	offsets.wqcfg = idxd_vfio_wqcfg_offset(vfio_dev) / IDXD_TABLE_MULT;
+	offsets.msix_perm = idxd_vfio_msix_perm_offset(vfio_dev) / IDXD_TABLE_MULT;
+	offsets.ims = idxd_vfio_ims_offset(vfio_dev) / IDXD_TABLE_MULT;
 
 	return offsets.bits[idx];
 }
 
-static bool idxd_vfio_wqcfg_readb(struct idxd_vdev *ivdev, loff_t pos, u8 *val)
+static bool idxd_vfio_wqcfg_readb(struct idxd_vfio_device *vfio_dev,
+				  loff_t pos, u8 *val)
 {
+	struct idxd_vdev *ivdev = vfio_dev->ivdev;
 	struct idxd_device *idxd = ivdev->idxd;
 	struct idxd_vwq *vwq;
 	union wqcfg wqcfg;
+	u32 wqcfg_offset = idxd_vfio_wqcfg_offset(vfio_dev);
 	unsigned int id;
 	u32 offset;
 
-	if (pos < IDXD_VDEV_WQCFG_OFFSET)
+	if (pos < wqcfg_offset ||
+	    pos >= wqcfg_offset + idxd_vfio_wqcfg_size(vfio_dev))
 		return false;
 
-	offset = pos - IDXD_VDEV_WQCFG_OFFSET;
+	offset = pos - wqcfg_offset;
 	id = offset / idxd->wqcfg_size;
 	if (id >= ivdev->num_wqs)
 		return false;
@@ -173,13 +240,33 @@ static bool idxd_vfio_wqcfg_readb(struct idxd_vdev *ivdev, loff_t pos, u8 *val)
 	if (!vwq)
 		return false;
 
-	memcpy(&wqcfg, vwq->wq->wqcfg, sizeof(wqcfg));
+	memcpy(&wqcfg, &vfio_dev->wqcfg[id], sizeof(wqcfg));
 	wqcfg.mode_support = !vwq->shared;
+	wqcfg.wq_state = test_bit(id, vfio_dev->wq_enable_map) ?
+		IDXD_WQ_ENABLED : IDXD_WQ_DISABLED;
 	offset %= idxd->wqcfg_size;
 	if (offset >= sizeof(wqcfg))
 		*val = 0;
 	else
 		*val = ((u8 *)&wqcfg)[offset];
+
+	return true;
+}
+
+static bool idxd_vfio_grpcfg_readb(struct idxd_vfio_device *vfio_dev,
+				   loff_t pos, u8 *val)
+{
+	u32 grpcfg_offset = idxd_vfio_grpcfg_offset(vfio_dev);
+	u32 offset;
+
+	if (pos < grpcfg_offset || pos >= grpcfg_offset + GRPCFG_SIZE)
+		return false;
+
+	offset = pos - grpcfg_offset;
+	if (offset >= sizeof(vfio_dev->grpcfg))
+		*val = 0;
+	else
+		*val = ((u8 *)&vfio_dev->grpcfg)[offset];
 
 	return true;
 }
@@ -208,16 +295,34 @@ static u64 idxd_vfio_bar0_readq(struct idxd_vfio_device *vfio_dev, loff_t pos)
 		return idxd->hw.opcap.bits[(pos - IDXD_OPCAP_OFFSET) /
 					    sizeof(u64)];
 	case IDXD_TABLE_OFFSET:
-		return idxd_vfio_read_table(0);
+		return idxd_vfio_read_table(vfio_dev, 0);
 	case IDXD_TABLE_OFFSET + sizeof(u64):
-		return idxd_vfio_read_table(1);
+		return idxd_vfio_read_table(vfio_dev, 1);
 	case IDXD_GENCFG_OFFSET:
 		return vfio_dev->gencfg;
 	case IDXD_GENCTRL_OFFSET:
 		return vfio_dev->genctrl;
 	case IDXD_GENSTATS_OFFSET:
-		gensts.state = IDXD_DEVICE_STATE_DISABLED;
+		gensts.state = vfio_dev->state;
 		return gensts.bits;
+	case IDXD_INTCAUSE_OFFSET:
+		return vfio_dev->intcause;
+	case IDXD_CMDSTS_OFFSET:
+		return vfio_dev->cmdsts;
+	case IDXD_CMDCAP_OFFSET:
+		return IDXD_VDEV_CMDCAP;
+	case IDXD_EVLCFG_OFFSET:
+		return vfio_dev->evlcfg[0];
+	case IDXD_EVLCFG_OFFSET + sizeof(u64):
+		return vfio_dev->evlcfg[1];
+	case IDXD_EVLSTATUS_OFFSET:
+		return vfio_dev->evlstatus;
+	case IDXD_SWERR_OFFSET:
+	case IDXD_SWERR_OFFSET + sizeof(u64):
+	case IDXD_SWERR_OFFSET + 2 * sizeof(u64):
+	case IDXD_SWERR_OFFSET + 3 * sizeof(u64):
+	case IDXD_IAACAP_OFFSET:
+		return 0;
 	default:
 		return 0;
 	}
@@ -228,7 +333,10 @@ static u8 idxd_vfio_bar0_readb(struct idxd_vfio_device *vfio_dev, loff_t pos)
 	u64 val;
 	u8 byte;
 
-	if (idxd_vfio_wqcfg_readb(vfio_dev->ivdev, pos, &byte))
+	if (idxd_vfio_wqcfg_readb(vfio_dev, pos, &byte))
+		return byte;
+
+	if (idxd_vfio_grpcfg_readb(vfio_dev, pos, &byte))
 		return byte;
 
 	val = idxd_vfio_bar0_readq(vfio_dev, pos & ~0x7ULL);
@@ -244,14 +352,285 @@ static ssize_t idxd_vfio_bar0_read(struct idxd_vfio_device *vfio_dev,
 		return -EINVAL;
 
 	count = min_t(size_t, count, IDXD_VDEV_BAR0_SIZE - pos);
+	mutex_lock(&vfio_dev->bar0_lock);
 	for (done = 0; done < count; done++) {
 		u8 val = idxd_vfio_bar0_readb(vfio_dev, pos + done);
 
-		if (copy_to_user(buf + done, &val, sizeof(val)))
+		if (copy_to_user(buf + done, &val, sizeof(val))) {
+			mutex_unlock(&vfio_dev->bar0_lock);
 			return -EFAULT;
+		}
 	}
+	mutex_unlock(&vfio_dev->bar0_lock);
 
 	return count;
+}
+
+static void idxd_vfio_init_wqcfg(struct idxd_vfio_device *vfio_dev)
+{
+	struct idxd_vdev *ivdev = vfio_dev->ivdev;
+	struct idxd_vwq *vwq;
+
+	list_for_each_entry(vwq, &ivdev->wqs, node) {
+		union wqcfg *wqcfg = &vfio_dev->wqcfg[vwq->id];
+
+		memcpy(wqcfg, vwq->wq->wqcfg, sizeof(*wqcfg));
+		wqcfg->pasid = 0;
+		wqcfg->pasid_en = 0;
+		wqcfg->mode_support = !vwq->shared;
+		wqcfg->wq_state = IDXD_WQ_DISABLED;
+		if (vwq->shared)
+			wqcfg->mode = 0;
+		else
+			wqcfg->mode = 1;
+	}
+}
+
+static void idxd_vfio_init_grpcfg(struct idxd_vfio_device *vfio_dev)
+{
+	struct idxd_vdev *ivdev = vfio_dev->ivdev;
+	struct idxd_vwq *vwq;
+	unsigned int engines;
+
+	memset(&vfio_dev->grpcfg, 0, sizeof(vfio_dev->grpcfg));
+	list_for_each_entry(vwq, &ivdev->wqs, node)
+		vfio_dev->grpcfg.wqs[vwq->id / 64] |= BIT_ULL(vwq->id % 64);
+
+	engines = min_t(unsigned int, ivdev->num_wqs,
+			ivdev->idxd->hw.engine_cap.num_engines);
+	engines = min_t(unsigned int, engines, 64);
+	if (engines)
+		vfio_dev->grpcfg.engines = GENMASK_ULL(engines - 1, 0);
+	vfio_dev->grpcfg.flags.tc_a = 0;
+	vfio_dev->grpcfg.flags.tc_b = 1;
+}
+
+static void idxd_vfio_reset_bar0(struct idxd_vfio_device *vfio_dev)
+{
+	vfio_dev->state = IDXD_DEVICE_STATE_DISABLED;
+	vfio_dev->genctrl = 0;
+	vfio_dev->gencfg = 0;
+	vfio_dev->intcause = 0;
+	vfio_dev->cmdsts = IDXD_CMDSTS_SUCCESS;
+	vfio_dev->evlcfg[0] = 0;
+	vfio_dev->evlcfg[1] = 0;
+	vfio_dev->evlstatus = 0;
+	bitmap_zero(vfio_dev->wq_enable_map, vfio_dev->ivdev->num_wqs);
+	idxd_vfio_init_wqcfg(vfio_dev);
+	idxd_vfio_init_grpcfg(vfio_dev);
+}
+
+static u32 idxd_vfio_cmdsts(u8 err, u16 result)
+{
+	union cmdsts_reg cmdsts = {};
+
+	cmdsts.err = err;
+	cmdsts.result = result;
+	return cmdsts.bits;
+}
+
+static void idxd_vfio_complete_cmd(struct idxd_vfio_device *vfio_dev,
+				   union idxd_command_reg cmd, u8 err,
+				   u16 result)
+{
+	vfio_dev->cmdsts = idxd_vfio_cmdsts(err, result);
+	if (cmd.int_req)
+		vfio_dev->intcause |= IDXD_INTC_CMD;
+}
+
+static u8 idxd_vfio_enable_wq(struct idxd_vfio_device *vfio_dev, u32 operand)
+{
+	struct idxd_vwq *vwq;
+	union wqcfg *wqcfg;
+
+	if (operand >= vfio_dev->ivdev->num_wqs)
+		return IDXD_CMDSTS_INVAL_WQIDX;
+	if (vfio_dev->state != IDXD_DEVICE_STATE_ENABLED)
+		return IDXD_CMDSTS_ERR_DEV_NOTEN;
+	if (test_bit(operand, vfio_dev->wq_enable_map))
+		return IDXD_CMDSTS_ERR_WQ_ENABLED;
+
+	vwq = idxd_vfio_vwq(vfio_dev->ivdev, operand);
+	if (!vwq)
+		return IDXD_CMDSTS_INVAL_WQIDX;
+
+	wqcfg = &vfio_dev->wqcfg[operand];
+	if (vwq->shared && wqcfg->mode != 0)
+		return IDXD_CMDSTS_ERR_WQ_MODE;
+	if (wqcfg->wq_size == 0)
+		return IDXD_CMDSTS_ERR_WQ_SIZE;
+	if (!wqcfg->mode && wqcfg->wq_thresh == 0)
+		return IDXD_CMDSTS_ERR_WQ_SIZE;
+
+	set_bit(operand, vfio_dev->wq_enable_map);
+	return IDXD_CMDSTS_SUCCESS;
+}
+
+static u8 idxd_vfio_disable_wq(struct idxd_vfio_device *vfio_dev, u32 operand)
+{
+	unsigned long mask = operand & GENMASK(15, 0);
+	unsigned int bit, base = (operand >> 16) * 16;
+
+	if (!mask)
+		return IDXD_CMDSTS_INVAL_WQIDX;
+	if (vfio_dev->state != IDXD_DEVICE_STATE_ENABLED)
+		return IDXD_CMDSTS_ERR_DEV_NOT_EN;
+
+	for_each_set_bit(bit, &mask, 16) {
+		unsigned int id = base + bit;
+
+		if (id >= vfio_dev->ivdev->num_wqs)
+			return IDXD_CMDSTS_INVAL_WQIDX;
+		clear_bit(id, vfio_dev->wq_enable_map);
+	}
+
+	return IDXD_CMDSTS_SUCCESS;
+}
+
+static u8 idxd_vfio_reset_wq(struct idxd_vfio_device *vfio_dev, u32 operand)
+{
+	struct idxd_vwq *vwq;
+	unsigned long mask = operand & GENMASK(15, 0);
+	unsigned int bit, base = (operand >> 16) * 16;
+
+	if (!mask)
+		return IDXD_CMDSTS_INVAL_WQIDX;
+	if (vfio_dev->state != IDXD_DEVICE_STATE_ENABLED)
+		return IDXD_CMDSTS_ERR_DEV_NOT_EN;
+
+	for_each_set_bit(bit, &mask, 16) {
+		unsigned int id = base + bit;
+
+		if (id >= vfio_dev->ivdev->num_wqs)
+			return IDXD_CMDSTS_INVAL_WQIDX;
+
+		vwq = idxd_vfio_vwq(vfio_dev->ivdev, id);
+		if (!vwq)
+			return IDXD_CMDSTS_INVAL_WQIDX;
+
+		clear_bit(id, vfio_dev->wq_enable_map);
+		memcpy(&vfio_dev->wqcfg[id], vwq->wq->wqcfg,
+		       sizeof(vfio_dev->wqcfg[id]));
+		vfio_dev->wqcfg[id].pasid = 0;
+		vfio_dev->wqcfg[id].pasid_en = 0;
+		vfio_dev->wqcfg[id].mode_support = !vwq->shared;
+		vfio_dev->wqcfg[id].wq_state = IDXD_WQ_DISABLED;
+		vfio_dev->wqcfg[id].mode = vwq->shared ? 0 : 1;
+	}
+
+	return IDXD_CMDSTS_SUCCESS;
+}
+
+static u8 idxd_vfio_check_wq_mask(struct idxd_vfio_device *vfio_dev, u32 operand)
+{
+	unsigned long mask = operand & GENMASK(15, 0);
+	unsigned int bit, base = (operand >> 16) * 16;
+
+	if (!mask)
+		return IDXD_CMDSTS_INVAL_WQIDX;
+	if (vfio_dev->state != IDXD_DEVICE_STATE_ENABLED)
+		return IDXD_CMDSTS_ERR_DEV_NOT_EN;
+
+	for_each_set_bit(bit, &mask, 16) {
+		if (base + bit >= vfio_dev->ivdev->num_wqs)
+			return IDXD_CMDSTS_INVAL_WQIDX;
+	}
+
+	return IDXD_CMDSTS_SUCCESS;
+}
+
+static void idxd_vfio_exec_cmd(struct idxd_vfio_device *vfio_dev, u32 val)
+{
+	union idxd_command_reg cmd = { .bits = val };
+	u8 status = IDXD_CMDSTS_SUCCESS;
+
+	switch (cmd.cmd) {
+	case IDXD_CMD_ENABLE_DEVICE:
+		if (vfio_dev->state == IDXD_DEVICE_STATE_ENABLED)
+			status = IDXD_CMDSTS_ERR_DEV_ENABLED;
+		else
+			vfio_dev->state = IDXD_DEVICE_STATE_ENABLED;
+		break;
+	case IDXD_CMD_DISABLE_DEVICE:
+		vfio_dev->state = IDXD_DEVICE_STATE_DISABLED;
+		bitmap_zero(vfio_dev->wq_enable_map, vfio_dev->ivdev->num_wqs);
+		break;
+	case IDXD_CMD_RESET_DEVICE:
+		idxd_vfio_reset_bar0(vfio_dev);
+		break;
+	case IDXD_CMD_ENABLE_WQ:
+		status = idxd_vfio_enable_wq(vfio_dev, cmd.operand);
+		break;
+	case IDXD_CMD_DISABLE_WQ:
+		status = idxd_vfio_disable_wq(vfio_dev, cmd.operand);
+		break;
+	case IDXD_CMD_DRAIN_WQ:
+	case IDXD_CMD_ABORT_WQ:
+		status = idxd_vfio_check_wq_mask(vfio_dev, cmd.operand);
+		break;
+	case IDXD_CMD_RESET_WQ:
+		status = idxd_vfio_reset_wq(vfio_dev, cmd.operand);
+		break;
+	case IDXD_CMD_DRAIN_ALL:
+	case IDXD_CMD_ABORT_ALL:
+	case IDXD_CMD_DRAIN_PASID:
+	case IDXD_CMD_ABORT_PASID:
+		break;
+	default:
+		status = IDXD_CMDSTS_INVAL_CMD;
+		break;
+	}
+
+	idxd_vfio_complete_cmd(vfio_dev, cmd, status, 0);
+}
+
+static bool idxd_vfio_wqcfg_write(struct idxd_vfio_device *vfio_dev,
+				  loff_t pos, u32 val)
+{
+	struct idxd_device *idxd = vfio_dev->ivdev->idxd;
+	struct idxd_vwq *vwq;
+	union wqcfg *wqcfg;
+	u32 wqcfg_offset = idxd_vfio_wqcfg_offset(vfio_dev);
+	u32 offset;
+	unsigned int id, idx;
+	u32 mask;
+
+	if (pos < wqcfg_offset ||
+	    pos >= wqcfg_offset + idxd_vfio_wqcfg_size(vfio_dev))
+		return false;
+	if (!IS_ALIGNED(pos, sizeof(u32)))
+		return true;
+
+	offset = pos - wqcfg_offset;
+	id = offset / idxd->wqcfg_size;
+	idx = (offset % idxd->wqcfg_size) / sizeof(u32);
+	if (id >= vfio_dev->ivdev->num_wqs ||
+	    idx >= ARRAY_SIZE(vfio_dev->wqcfg[0].bits))
+		return true;
+
+	vwq = idxd_vfio_vwq(vfio_dev->ivdev, id);
+	if (!vwq)
+		return true;
+
+	wqcfg = &vfio_dev->wqcfg[id];
+	if (vwq->shared || test_bit(id, vfio_dev->wq_enable_map))
+		return true;
+
+	switch (idx) {
+	case 1:
+		mask = GENMASK(15, 0);
+		break;
+	case 2:
+		mask = IDXD_VDEV_WQCFG_IDX2_WR_MASK;
+		break;
+	default:
+		mask = 0;
+		break;
+	}
+
+	wqcfg->bits[idx] = (wqcfg->bits[idx] & ~mask) | (val & mask);
+	wqcfg->mode_support = 1;
+	return true;
 }
 
 static ssize_t idxd_vfio_bar0_write(struct idxd_vfio_device *vfio_dev,
@@ -259,21 +638,43 @@ static ssize_t idxd_vfio_bar0_write(struct idxd_vfio_device *vfio_dev,
 				    loff_t pos)
 {
 	u32 val;
+	u64 val64;
 
 	if (pos < 0 || pos >= IDXD_VDEV_BAR0_SIZE)
 		return -EINVAL;
 
-	if (count == sizeof(val) &&
-	    (pos == IDXD_GENCFG_OFFSET || pos == IDXD_GENCTRL_OFFSET)) {
+	mutex_lock(&vfio_dev->bar0_lock);
+	if (count == sizeof(val)) {
 		if (copy_from_user(&val, buf, sizeof(val)))
-			return -EFAULT;
+			goto err_fault;
+		if (idxd_vfio_wqcfg_write(vfio_dev, pos, val))
+			goto out_unlock;
 		if (pos == IDXD_GENCFG_OFFSET)
 			vfio_dev->gencfg = val;
-		else
+		else if (pos == IDXD_GENCTRL_OFFSET)
 			vfio_dev->genctrl = val;
+		else if (pos == IDXD_INTCAUSE_OFFSET)
+			vfio_dev->intcause &= ~val;
+		else if (pos == IDXD_CMD_OFFSET)
+			idxd_vfio_exec_cmd(vfio_dev, val);
+		else if (pos == IDXD_EVLSTATUS_OFFSET)
+			vfio_dev->evlstatus = val;
+	} else if (count == sizeof(val64)) {
+		if (copy_from_user(&val64, buf, sizeof(val64)))
+			goto err_fault;
+		if (pos == IDXD_EVLCFG_OFFSET)
+			vfio_dev->evlcfg[0] = val64;
+		else if (pos == IDXD_EVLCFG_OFFSET + sizeof(u64))
+			vfio_dev->evlcfg[1] = val64;
 	}
 
+out_unlock:
+	mutex_unlock(&vfio_dev->bar0_lock);
 	return min_t(size_t, count, IDXD_VDEV_BAR0_SIZE - pos);
+
+err_fault:
+	mutex_unlock(&vfio_dev->bar0_lock);
+	return -EFAULT;
 }
 
 static int idxd_vfio_bar2_pos(struct idxd_vfio_device *vfio_dev, loff_t pos,
@@ -648,6 +1049,32 @@ static const struct vfio_device_ops idxd_vfio_ops = {
 	.pasid_detach_ioas = idxd_vfio_pasid_detach_ioas,
 };
 
+static int idxd_vfio_init_bar0(struct idxd_vfio_device *vfio_dev)
+{
+	struct idxd_vdev *ivdev = vfio_dev->ivdev;
+
+	vfio_dev->wqcfg = kcalloc(ivdev->num_wqs, sizeof(*vfio_dev->wqcfg),
+				  GFP_KERNEL);
+	if (!vfio_dev->wqcfg)
+		return -ENOMEM;
+
+	vfio_dev->wq_enable_map = bitmap_zalloc(ivdev->num_wqs, GFP_KERNEL);
+	if (!vfio_dev->wq_enable_map) {
+		kfree(vfio_dev->wqcfg);
+		vfio_dev->wqcfg = NULL;
+		return -ENOMEM;
+	}
+
+	idxd_vfio_reset_bar0(vfio_dev);
+	return 0;
+}
+
+static void idxd_vfio_free_bar0(struct idxd_vfio_device *vfio_dev)
+{
+	bitmap_free(vfio_dev->wq_enable_map);
+	kfree(vfio_dev->wqcfg);
+}
+
 static int idxd_vfio_probe(struct idxd_dev *idxd_dev)
 {
 	struct idxd_vdev *ivdev = idxd_dev_to_vdev(idxd_dev);
@@ -661,20 +1088,28 @@ static int idxd_vfio_probe(struct idxd_dev *idxd_dev)
 		return PTR_ERR(vfio_dev);
 
 	vfio_dev->ivdev = ivdev;
+	mutex_init(&vfio_dev->bar0_lock);
 	mutex_init(&vfio_dev->pasid_lock);
 	idxd_vfio_init_config(vfio_dev);
 	dev_set_drvdata(dev, vfio_dev);
 
+	rc = idxd_vfio_init_bar0(vfio_dev);
+	if (rc)
+		goto err_drvdata;
+
 	rc = vfio_register_emulated_iommu_dev(&vfio_dev->vdev);
 	if (rc)
-		goto err_put;
+		goto err_bar0;
 
 	dev_info(dev, "registered VFIO VDEV with %u WQ(s)\n", ivdev->num_wqs);
 	return 0;
 
-err_put:
+err_bar0:
+	idxd_vfio_free_bar0(vfio_dev);
+err_drvdata:
 	dev_set_drvdata(dev, NULL);
 	mutex_destroy(&vfio_dev->pasid_lock);
+	mutex_destroy(&vfio_dev->bar0_lock);
 	vfio_put_device(&vfio_dev->vdev);
 	return rc;
 }
@@ -687,7 +1122,9 @@ static void idxd_vfio_remove(struct idxd_dev *idxd_dev)
 
 	dev_set_drvdata(dev, NULL);
 	vfio_unregister_group_dev(&vfio_dev->vdev);
+	idxd_vfio_free_bar0(vfio_dev);
 	mutex_destroy(&vfio_dev->pasid_lock);
+	mutex_destroy(&vfio_dev->bar0_lock);
 	vfio_put_device(&vfio_dev->vdev);
 }
 
